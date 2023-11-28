5a3201555b27871445ad6249903fbca106019b28
Nelamin2 <nidalelamin.com>
2023-11-27 18:39:27 -0800

    task10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,10 +12,12 @@
  */
 int check_cycle(listint_t *list)
 {
+listint_t *slow;
+listint_t *fast;
 if (list == NULL || list->next == NULL)
 return (0);
-listint_t *slow = list;
-listint_t *fast = list->next;
+slow = list;
+fast = list->next;
 while (fast != NULL && fast->next != NULL)
 {
 if (slow == fast)
