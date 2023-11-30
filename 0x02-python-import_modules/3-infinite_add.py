22b8bf56649d38b16fc321aee7a969f57d5fcdec
Nelamin2 <nidalelamin.com>
2023-11-30 15:58:45 -0800

    task4

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -9,7 +9,7 @@ def infinite_add(argv):
         while i <= argc:
             add += int(argv[i])
             i+=1
-        print("{:d}".format(add))
+            print("{:d}".format(add))
 
 if __name__ == "__main__":
     import sys
