d15d3c06e7646b7aa93169fb7a8e64d367643134
Nelamin2 <nidalelamin.com>
2023-11-30 10:47:45 -0800

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg = sys.argv
+    argc = len(sys.argv) - 1
+    if argc > 1:
+        print("{} arguments:".format(argc))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, arg[i]))
+
+    elif argc == 0:
+        print("{} arguments.".format(argc))
+
+    else:
+        print("{} argument:".format(argc))
+        print("{}: {}".format(argc, arg[1]))
+
