ae002dfd43b9c6d479d2f1a6bf98c354384cc592
Nelamin2 <nidalelamin.com>
2023-12-04 13:33:19 -0800

    task7

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-   elements = (len(sentence), None if not sentence else sentence[0])
-   return elements
+    elements = (len(sentence), None if not sentence else sentence[0])
+    return elements
