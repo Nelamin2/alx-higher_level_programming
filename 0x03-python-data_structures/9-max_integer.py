c1720596c1424b12a742055aad964c8c6a46b3ca
Nelamin2 <nidalelamin.com>
2023-12-04 13:42:49 -0800

    task9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def max_integer(my_list=None):
-    if my_list is None:
-        my_list = []
-    
-    my_list.sort()
-    my_list.reverse()
-    return my_list[0]
+def max_integer(my_list=[]):
+    new_list = []
+    if my_list:
+        my_list.sort()
+        my_list.reverse()
+        return (my_list[0])
+    return (None)
