6f5997405810efe503202d5905aaa15cb7c8b284
Nelamin2 <nidalelamin.com>
2023-12-03 18:47:58 -0800

    task12

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    new_list = []
-    for num in my_list:
-        new_list.append(False if num % 2 else True)
+def divisible_by_2(my_list=None):
+    if my_list is None:
+        my_list = []
+
+    new_list = [False if num % 2 else True for num in my_list]
     return new_list
+
