c6a06a00adb2f720f2975cb8a2c8d77e259b1d46
Nelamin2 <nidalelamin.com>
2023-12-03 19:19:01 -0800

    tasks

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
+def print_matrix_integer(matrix=None):
+    if matrix is None:
+        matrix = [[]]
+
     for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                if j != len(matrix[i]) - 1:
-                    endspace = ' '
-                else:
-                    endspace = ''
-                print("{:d}".format(matrix[i][j]), end=endspace)
-            print()
+        for j in range(len(matrix[i])):
+            if j != len(matrix[i]) - 1:
+                endspace = ' '
+            else:
+                endspace = ''
+            print("{:d}".format(matrix[i][j]), end=endspace)
+        print()
