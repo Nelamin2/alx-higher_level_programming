7753e7d91e8d27efe53a41dc5aa4fc75c2bda18f
Nelamin2 <nidalelamin@gmail.com>
2023-12-19 16:26:33 -0800

    task7

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
 import math
-
+"""Define a MagicClass matching exactly a bytecode provided by Holberton."""
 class MagicClass:
+    """Represent a circle."""
     def __init__(self, radius=0):
+        """Initialize a MagicClass
+        Arg:
+            radius (float or int): The radius of the new MagicClass."""
         self.__radius = 0
+         if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
         self.radius = radius
-
-    @property
-    def radius(self):
-        return self.__radius
-
-    @radius.setter
-    def radius(self, value):
-        if type(value) is not int and type(value) is not float:
-            raise TypeError('radius must be a number')
-        self.__radius = value
 
     def area(self):
         return self.__radius ** 2 * math.pi
-
+        """Return the area of the MagicClass."""
     def circumference(self):
         return 2 * math.pi * self.__radius
-
+        """Return The circumference of the MagicClass."""
