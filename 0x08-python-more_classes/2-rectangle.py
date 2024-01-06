b24ec986c7dffff58a1766d04770bfa1d6a4dab6
Nelamin2 <nidalelamin@gmail.com>
2024-01-06 10:21:30 -0800

    task2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""Define a Class."""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """Represents a Class."""
+    """Represent a rectangle."""
 
-    def __init__(self, width = 0, height = 0):
-        """Initialize a new Rectangle."""
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -25,7 +30,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
+        """Get/set the height of the Rectangle."""
         return self.__height
 
     @height.setter
