a054eed3c6b3140339959a0fdc941a869e4bd999
Nelamin2 <nidalelamin@gmail.com>
2024-01-06 10:01:29 -0800

    task2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -42,3 +42,9 @@ class Rectangle:
          """Return the area of the Rectangle."""
         return (self.__width * self.__height)
 
+    def perimeter(self):
+        """Return the perimeter of the Rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
