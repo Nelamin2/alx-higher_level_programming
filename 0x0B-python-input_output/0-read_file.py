6ad027f44e950b502136222579c2534b0eabcd6f
Nelamin2 <nidalelamin@gmail.com>
2024-01-09 11:22:23 -0800

    tasks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,7 @@
 
 def read_file(filename=""):
     """a funtion to Print the contents of a UTF8 text file to stdout."""
+
+
 with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
