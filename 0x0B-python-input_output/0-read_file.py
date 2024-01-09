86cada5f3645cdfc936035d83ddbbec9276cf299
Nelamin2 <nidalelamin@gmail.com>
2024-01-09 12:43:55 -0800

    task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""a module for a text file-reading function."""
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    """a funtion to Print the contents of a UTF8 text file to stdout."""
-    try:
-        with open(filename, encoding="utf-8") as file:
-            content = file.read()
-            print(content, end="")
-    except FileNotFoundError:
-        pass
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
