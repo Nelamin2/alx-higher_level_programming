773e13b9b46c6a66816f739a42d4cd13e60b0832
Nelamin2 <nidalelamin@gmail.com>
2024-01-08 16:12:34 -0800

    task13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def add_attribute(obj, attribute, value):
+    """Add attribute to object if possible.
+
+    Args:
+        obj: The object.
+        attribute: The attribute to add.
+        value: The value for the attribute.
+
+    Raises:
+        TypeError: If the attribute cannot be added.
+    """
+    if hasattr(obj, "__dict__") or (hasattr(obj, "__slots__") and attribute in obj.__slots__):
+        setattr(obj, attribute, value)
+    else:
+        raise TypeError("can't add new attribute")
