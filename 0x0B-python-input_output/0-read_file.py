2ece662c07c7723f40adaf834940cadae800a894
Nelamin2 <nidalelamin@gmail.com>
2024-01-09 11:55:32 -0800

    task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,11 @@
 
 def read_file(filename=""):
     """a funtion to Print the contents of a UTF8 text file to stdout."""
-with open(filename, encoding="utf-8") as f:
-    print(f.read(), end="")
+
+
+    try:
+        with open(filename, encoding="utf-8") as file:
+            content = file.read()
+            print(content, end="")
+    except FileNotFoundError:
+        pass
