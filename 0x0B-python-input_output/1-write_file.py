33e502a92d7c4ecaeacbeb731ab515fb85608fef
Nelamin2 <nidalelamin@gmail.com>
2024-01-09 11:45:14 -0800

    task0

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -12,4 +12,4 @@ def write_file(filename="", text=""):
         The number of characters written.
     """
     with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)#!/usr/bin/python3
+        return f.write(text)
