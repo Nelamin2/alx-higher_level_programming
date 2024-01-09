9367f0f9422208b9dcbb79fad05c176133e3e7e6
Nelamin2 <nidalelamin@gmail.com>
2024-01-09 11:38:28 -0800

    task0

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,7 +4,6 @@
 
 def write_file(filename="", text=""):
     """ a function to Write a string to a UTF8 text file.
-
     Args:
         filename (str): The name of the file to write.
         text (str): The text to write to the file.
@@ -13,5 +12,5 @@ def write_file(filename="", text=""):
     """
 
 
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+with open(filename, "w", encoding="utf-8") as f:
+    return f.write(text)
