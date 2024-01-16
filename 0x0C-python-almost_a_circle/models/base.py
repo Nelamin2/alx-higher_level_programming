3829b96608f3568d4427436ed590365286392637
Nelamin2 <nidalelamin@gmail.com>
2024-01-16 03:50:32 +0300

    task16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,7 +34,39 @@ class Base:
             return []
         return loads(json_string)
 
-  @classmethod
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''Saves jsonified object to file.'''
+        if list_objs is not None:
+            list_objs = [o.to_dictionary() for o in list_objs]
+        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_objs))
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''Loads instance from dictionary.'''
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls is Rectangle:
+            new = Rectangle(1, 1)
+        elif cls is Square:
+            new = Square(1)
+        else:
+            new = None
+        new.update(**dictionary)
+        return new
+
+    @classmethod
+    def load_from_file(cls):
+        '''Loads string from file and unjsonifies.'''
+        from os import path
+        file = "{}.json".format(cls.__name__)
+        if not path.isfile(file):
+            return []
+        with open(file, "r", encoding="utf-8") as f:
+            return [cls.create(**d) for d in cls.from_json_string(f.read())]
+
+    @classmethod
     def save_to_file_csv(cls, list_objs):
         '''Saves object to csv file.'''
         from models.rectangle import Rectangle
@@ -56,7 +88,7 @@ class Base:
         '''Loads object to csv file.'''
         from models.rectangle import Rectangle
         from models.square import Square
-        file = []
+        ret = []
         with open('{}.csv'.format(cls.__name__), 'r', newline='',
                   encoding='utf-8') as f:
             reader = csv.reader(f)
@@ -68,6 +100,5 @@ class Base:
                 else:
                     d = {"id": row[0], "size": row[1],
                          "x": row[2], "y": row[3]}
-                file.append(cls.create(**d))
-        return file
-
+                ret.append(cls.create(**d))
+        return ret
