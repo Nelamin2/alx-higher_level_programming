fbdae6089a7e1ac84634e2e1b2526ad37d130f5f
Nelamin2 <nidalelamin@gmail.com>
2024-03-12 15:15:12 -0700

    tasks

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const { dict } = require('./101-data');
+
+let occurrences = {};
+
+for (let userId in dict) {
+  let occurrence = dict[userId];
+  if (occurrences[occurrence] === undefined) {
+    occurrences[occurrence] = [];
+  }
+  occurrences[occurrence].push(userId);
+}
+
+console.log(occurrences);
+
