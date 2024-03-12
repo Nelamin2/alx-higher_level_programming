47ae8801bfff030471de55f6698e52ab9f890b52
Nelamin2 <nidalelamin@gmail.com>
2024-03-12 14:44:24 -0700

    tasks

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Squareinstance = require('./5-square');
+
+class Square extends Squareinstance {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Square;
