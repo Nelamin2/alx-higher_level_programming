65b81ddb744e78c84780cc5df308f3eea9c73a3f
Nelamin2 <nidalelamin@gmail.com>
2024-03-27 16:03:57 -0700

    tasks

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # shellcheck disable=SC2046
-if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi#!/bin/bash
+if [ "$(curl -L -s -X HEAD -w "%{http_code}" "$1")" == '200' ]; then
+    curl -Ls "$1"
+fi
