82acd501d8ca6e9e9ee8a6fbf5c3165adc7f4060
Nelamin2 <nidalelamin@gmail.com>
2024-03-27 16:06:52 -0700

    tasks

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-response=$(mktemp)
-curl -sX GET -o "$response" "$1" -L
-size=$(wc -c < "$response")
-echo "Size of response body: $size bytes"
-rm "$response"
-
+curl -sX GET $1 -L
